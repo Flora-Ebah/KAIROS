06e1651d8b0a12f6baf164d150be40ac71fb9187
nandju <nlbmd17@gmail.com>
2025-03-25 20:54:48 +0000

    continue stage

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-    strictPort: true,
-    open: true
-  }
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendor'; // Regroupe React et ReactDOM
+            }
+            return 'vendor'; // Regroupe les autres dépendances
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 800, // Augmente la limite pour éviter l'avertissement
+  },
+});
